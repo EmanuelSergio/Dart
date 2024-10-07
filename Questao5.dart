187a768eab737338784d82da72c46ab8f9a38d30
William Morais <williamweidgenant@gmail.com>
2024-10-07 15:56:35 -0300

    Questão 5

---
+++ Questao5.dart
@@ -0,0 +1,8 @@
+void main(){
+  double salario = 2850.00;
+  double parcela = salario * 0.30;
+  double valorTotal = parcela/ (1 - 0.12);
+  print('salario é de R$salario');
+  print('parcela é de R$parcela');
+  print('o total do crédito concedido é de R$valorTotal');
+}
