d50d67179b6057e89a0ab94d9f54d1884d4d1fbf
EmanuelSergio <emanuelsergiogirardi@gmail.com>
2024-10-16 14:42:15 -0300

    ternario

---
+++ Flutter/OperdorTernario.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  int idade = 17;
+  bool maiorDeTodos = idade > 17 ? true : false;
+  print(maiorDeTodos);
+
+  bool minhaVariavel = true;
+
+  minhaVariavel == true ? print("esta Verdadeiro") : print("Esta falso");
+
+}
