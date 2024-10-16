4c958031b22658d360aca13a1f5e856cf72f8bfc
William Morais <williamweidgenant@gmail.com>
2024-10-16 14:10:59 -0300

    WhileCase

---
+++ Exercicios/Lista2/WhileCase.dart
@@ -0,0 +1,18 @@
+var numero = 10;
+
+void main(){
+while(numero >= 0){
+  print("Executando $numero");
+  numero--;
+  if(numero <= 0){
+    print("Recarregar");
+    break;
+  }
+}
+
+while(numero <= 10){
+  print("Recarregando $numero");
+  numero++;
+}
+
+}
