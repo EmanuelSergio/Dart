6601520750858cd19a06fcc83e13ce460c1dc715
William Morais <williamweidgenant@gmail.com>
2024-10-16 13:52:58 -0300

    Switch montado

---
+++ Exercicios/Lista2/Atividade1.dart
@@ -0,0 +1,27 @@
+void main(){
+
+var comando = "vazio";
+
+switch (comando) {
+  case "Cadastrar usuario":
+    
+    break;
+  case "Cadastrar cliente":
+
+      break;
+  case "Cadastrar produto":
+
+      break;
+  case "Listar usuarios":
+
+      break;
+  case "Listar clientes":
+
+      break;
+  case "Listar produtos":
+
+      break;
+  default:
+}
+
+}
