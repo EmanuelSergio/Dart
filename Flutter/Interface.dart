4f901e10e53a24c8e396feae9bf25fb8b43c9efb
William Morais <williamweidgenant@gmail.com>
2024-10-16 17:13:06 -0300

    commit

--- Flutter/Interface.dart
+++ Flutter/Interface.dart
@@ -1,5 +1,6 @@
 abstract class Animal{
   void fazerSom();
+  void velocidade();
 }
 
 class Cachorro implements Animal{
@@ -7,12 +8,20 @@ class Cachorro implements Animal{
   void fazerSom(){
     print("O cachorro faz au au");
   }
+   @override
+  void velocidade(){
+    print("O cachorro corre a até 30 km/h");
+  }
 }
 
 class Gato implements Animal{
   @override
   void fazerSom(){
     print("O gato faz miau");
+  }
+  @override
+  void velocidade(){
+    print("O gato corre a até 15 km/h");
   }
 }
 
